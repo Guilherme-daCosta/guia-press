94217f91a6c36e0ca58496252af50981ca3ff449
Guilherme-daCosta <gc.augusto@outlook.com>
2023-02-27 12:11:42 -0300

    feat: category delete action

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -30,7 +30,31 @@ router.post("/categories/save", (req, res) => {
 router.get("/admin/categories", (req, res) => {
     Category.findAll().then(categories => {
     res.render("admin/categories/index.ejs", { categories: categories });
-    })
+    });
+});
+
+
+router.post("/categories/delete", (req, res) => {
+    let id = req.body.id;
+
+    if (id != undefined) {
+        if (!isNaN(id)) {
+
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+
+        } else { // Não for número
+            res.redirect("/admin/categories");
+        }
+
+    } else { // Null
+        res.redirect("/admin/categories");
+    }
 });
 
 
