6f929a99a2c7339a70a06969947b40b784eb4a21
Guilherme-daCosta <gc.augusto@outlook.com>
2023-02-27 15:42:38 -0300

    feat: listing articles in page

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,7 +5,11 @@ const Article = require("./Article");
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
-    res.render("admin/articles/index.ejs");
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then(articles => {
+        res.render("admin/articles/index.ejs", { articles: articles });
+    });
 });
 
 
