be70a73f1a219e92d9f4c0ac42abcd76a3c83317
Guilherme-daCosta <gc.augusto@outlook.com>
2023-02-28 17:14:19 -0300

    feat: added logout

--- users/UsersController.js
+++ users/UsersController.js
@@ -72,4 +72,10 @@ router.post("/authenticate", (req, res) => {
 });
 
 
+router.get("/logout", (req, res) => {
+    req.session.user = undefined;
+    res.redirect("/");
+});
+
+
 module.exports = router;
